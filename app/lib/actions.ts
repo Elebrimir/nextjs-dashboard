c581b6539d6b8ddbf46463c24d34c3f67d3d2f62
Pablo Cortés <cortesoltra@gmail.com>
2024-01-14 20:19:15 +0100

    Chapter 15 Complete, don't log in

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const CreateInvoiceSchema = z.object({
   id: z.string(),
@@ -97,5 +99,26 @@ export async function deleteInvoice(id: string) {
   } catch (error) {
     console.log(`Error deleting invoice: ${error}`);
     return { message: 'Database Error: Failed to delete invoice' };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  console.log(formData);
+
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials';
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw error;
   }
 }
