23fd0ee7873991926f7b415ff8760434d7e7ad2c
Pablo Cortés <cortesoltra@gmail.com>
2024-01-16 00:06:54 +0100

    Customers Page Added

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,26 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import Table from '@/app/ui/customers/table';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function CustomersPage() {
-  return <p>Customer Page</p>;
+export default async function CustomersPage({
+  searchParams,
+}: {
+  searchParams?: { customers?: string };
+}) {
+  const customers = searchParams?.customers || '';
+
+  const totalcustomers = await fetchFilteredCustomers(customers);
+  return (
+    <div className="w-full">
+      <Suspense key={customers} fallback={<TableRowSkeleton />}>
+        <Table customers={totalcustomers} />
+      </Suspense>
+    </div>
+  );
 }
--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -40,12 +40,11 @@ export default async function InvoicesTable({
                   <InvoiceStatus status={invoice.status} />
                 </div>
                 <div className="flex w-full items-center justify-between pt-4">
-                  <div>
-                    <p className="text-xl font-medium">
-                      {formatCurrency(invoice.amount)}
-                    </p>
-                    <p>{formatDateToLocal(invoice.date)}</p>
-                  </div>
+                  <p className="text-xl font-medium">
+                    {formatCurrency(invoice.amount)}
+                  </p>
+                  <p>{formatDateToLocal(invoice.date)}</p>
+
                   <div className="flex justify-end gap-2">
                     <UpdateInvoice id={invoice.id} />
                     <DeleteInvoice id={invoice.id} />
