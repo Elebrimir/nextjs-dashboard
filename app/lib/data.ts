671a74fe2a96f4198a1c407a91a38ea7ae99aa58
Pablo Cortés <cortesoltra@gmail.com>
2024-01-15 17:44:43 +0100

    Data logs

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -73,6 +73,13 @@ export async function fetchCardData() {
     const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
 
+    console.log(
+      numberOfInvoices,
+      numberOfCustomers,
+      totalPaidInvoices,
+      totalPendingInvoices,
+    );
+
     return {
       numberOfCustomers,
       numberOfInvoices,
